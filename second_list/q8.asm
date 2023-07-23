4c9014ad3cc3412f8b80160ac69fc586fad0bc48
SageScroll18144 <ffss@cin.ufpe.br>
2023-07-23 09:11:57 -0300

    inicio da 8

--- second_list/q8.asm
+++ second_list/q8.asm
@@ -1,77 +1,129 @@
-lb x10, 0(x0)
-sb x10, 1030(x0)
-
 loop:
-	lh x10, 1031(x0)
+	# lh x10, 1031(x0)
 
-	lw t0, first_case
+    lw t0, in_0
+	blt x10, t0, if_zero
+
+	lw t0, in_1
 	blt x10, t0, if_one
 	
-    # lh x10, 1031(x0)
-	lw t0, second_case
+    lw t0, in_2
 	blt x10, t0, if_two
 	
-	# lh x10, 1031(x0)
-	lw t0, third_case
+    lw t0, in_3
 	blt x10, t0, if_three
 	
-	# lh x10, 1031(x0)
-	lw t0, fourth_case
+	lw t0, in_4
 	blt x10, t0, if_four
-
-	# lh x10, 1031(x0)
-	lw t0, fifth_case
+	
+    lw t0, in_5
 	blt x10, t0, if_five
+	
+    lw t0, in_6
+	blt x10, t0, if_six
+
+    lw t0, in_7
+	blt x10, t0, if_seven
+	
+    lw t0, in_8
+	blt x10, t0, if_eight
+	
+    lw t0, in_9
+	blt x10, t0, if_nine
 
 	# else 
-	# manda via serial o ultimo caso
-	lw x10, HIGH6
-	sb x10, 1029(x0)
-	lw x10, LOW
+	lw x10, low
 	sb x10, 1027(x0)
+	lw x10, low
+	sb x10, 1029(x0)
 	jal x11, loop 
 
-	if_one:
-		lw x10, HIGH1
+    if_zero:
+        lw x10, ans_0
 		sb x10, 1027(x0)
-		lw x10, LOW
+		lw x10, seg_g
+		sb x10, 1029(x0)
+		jal x11, loop 
+	if_one:
+		lw x10, ans_1
+		sb x10, 1027(x0)
+		lw x10, low
 		sb x10, 1029(x0)
 		jal x11, loop 
 	if_two:
-		lw x10, HIGH2
+		lw x10, ans_2
 		sb x10, 1027(x0)
-		lw x10, LOW
+		lw x10, seg_g
 		sb x10, 1029(x0)
 		jal x11, loop 
 	if_three:
-		lw x10, HIGH3
-		sb x10, 1029(x0)
-		lw x10, LOW
+		lw x10, ans_3
 		sb x10, 1027(x0)
+		lw x10, seg_g
+		sb x10, 1029(x0)
 		jal x11, loop  	
 	if_four:
-		lw x10, HIGH4
+		lw x10, ans_4
 		sb x10, 1027(x0)
-		lw x10, LOW
+		lw x10, seg_g
 		sb x10, 1029(x0)
 		jal x11, loop
 	if_five:
-		lw x10, HIGH5
-		sb x10, 1029(x0)
-		lw x10, LOW
+		lw x10, ans_5
 		sb x10, 1027(x0)
+		lw x10, seg_g
+		sb x10, 1029(x0)
 		jal x11, loop  
+    if_six:
+		lw x10, ans_6
+		sb x10, 1027(x0)
+		lw x10, seg_g
+		sb x10, 1029(x0)
+		jal x11, loop  
+    if_seven:
+		lw x10, ans_7
+		sb x10, 1027(x0)
+		lw x10, low
+		sb x10, 1029(x0)
+		jal x11, loop  
+    if_eight:
+		lw x10, ans_8
+		sb x10, 1027(x0)
+		lw x10, seg_g
+		sb x10, 1029(x0)
+		jal x11, loop 
+    if_nine:
+		lw x10, ans_9
+		sb x10, 1027(x0)
+		lw x10, seg_g
+		sb x10, 1029(x0)
+		jal x11, loop   
 
-first_case: .word 327
-second_case: .word 429
-third_case: .word 531
-fourth_case: .word 634
-fifth_case: .word 736
+in_0: .word 0
+in_1: .word 1
+in_2: .word 2 
+in_3: .word 3 
+in_4: .word 4 
+in_5: .word 5
+in_6: .word 6
+in_7: .word 7 
+in_8: .word 8 
+in_9: .word 9 
 
-HIGH1: .word 3
-HIGH2: .word 12
-HIGH3: .word 12
-HIGH4: .word 48
-HIGH5: .word 48
-HIGH6: .word 3
-LOW: .word 0
+# seg | pins
+# [a-f] : 8-13
+# g : 2
+
+ans_0: .word 0 # has seg 'g' 
+ans_1: .word 0
+ans_2: .word 0 # has seg 'g'
+ans_3: .word 0 # has seg 'g'
+ans_4: .word 0 # has seg 'g'
+ans_5: .word 0 # has seg 'g'
+ans_6: .word 0 # has seg 'g'
+ans_7: .word 0 
+ans_8: .word 0 # has seg 'g'
+ans_9: .word 0 # has seg 'g'
+
+seg_g: .word 1
+low: .word 0
