1fe9c1d839a9d49dc7aae76ff473b0a5ceace4ca
Mend25 <113634496+Mend25@users.noreply.github.com>
2023-07-23 06:54:57 -0300

    Falta implementar a função de printar
    Apresenta problemas para apresentar o resultado

--- second_list/q5.asm
+++ second_list/q5.asm
@@ -35,22 +35,22 @@ neg_a:
 
 save_1:
 	addi x5, x5, -48
-	sw x5, a1
+	sw x5, s6
 	jal x0, read_a
 
 save_2:
 	addi x5, x5, -48
-	sw x5, a2
+	sw x5, s7
 	jal x0, read_a
 
 save_3:
 	addi x5, x5, -48
-	sw x5, a3
+	sw x5, s8
 	jal x0, read_a
 
 save_4:
 	addi x5, x5, -48
-	sw x5, a4
+	sw x5, s9
 	jal x0, read_a
 
 prep_a:
@@ -64,64 +64,64 @@ prep_a:
 	beq x6, x7, thous_a
 
 unit_a:
-	add x9, x9, a1
+	add x9, x9, s6
 	jal x0, read_b
 
 tens_a:	
-	add x9, x9, a1
+	add x9, x9, s6
 	addi x10, x10, 1
 	lw x6, ten
 	beq x10, x6, mrg_2a
 	jal x0, tens_a
 
 mrg_2a:
-	add x9, x9, a2
+	add x9, x9, s7
 	jal x0, read_b
 
 hund_a:
-	add x9, x9, a1
+	add x9, x9, s6
 	addi x10, x10, 1
 	lw x6, one_hundred
-	beq x10, x6, tens_a2
+	beq x10, x6, tens_s7
 	jal x0, hund_a
 
-tens_a2:
-	add x11, x11, a2
+tens_s7:
+	add x11, x11, s7
 	addi x12, x12, 1
 	lw x6, ten
 	beq x12, x6, mrg_3a
-	jal x0, tens_a2
+	jal x0, tens_s7
 
 mrg_3a:
 	add x9, x9, x11
-	add x9, x9, a3
+	add x9, x9, s8
 	jal x0, read_b
 
 thous_a:
-	add x9, x9, a1
+	add x9, x9, s6
 	addi x10, x10, 1
 	lw x6, one_thousnd
-	beq x10, x6, hund_a2
+	beq x10, x6, hund_s7
 	jal x0, thous_a
 
-hund_a2:
-	add x11, x11, a3
+hund_s7:
+	add x11, x11, s8
 	addi x12, x12, 1
 	lw x6, one_hundred
-	beq x12, x6, tens_a3
-	jal x0, hund_a2
+	beq x12, x6, tens_s8
+	jal x0, hund_s7
 
-tens_a3:
-	add x13, x13, a4
+tens_s8:
+	add x13, x13, s9
 	addi x14, x14, 1
 	lw x6, ten
 	beq x14, x6, mrg_4a
-	jal x0, tens_a3
+	jal x0, tens_s8
 
 mrg_4a:
 	add x9, x9, x11
 	add x9, x9, x13
-	add x9, x9, a4
+	add x9, x9, s9
 	jal x0, read_b
 
 read_b:	
@@ -147,22 +147,22 @@ neg_b:
 
 save_1b:
 	addi x5, x5, -48
-	sw x5, t1
+	sw x5, t5
 	jal x0, read_b
 
 save_2b:
 	addi x5, x5, -48
-	sw x5, t2
+	sw x5, t6
 	jal x0, read_b
 
 save_3b:
 	addi x5, x5, -48
-	sw x5, t3
+	sw x5, t5
 	jal x0, read_b
 
 save_4b:
 	addi x5, x5, -48
-	sw x5, t4
+	sw x5, t6
 	jal x0, read_b
 
 prep_b:
@@ -176,29 +176,29 @@ prep_b:
 	beq x6, x8, thous_b
 
 unit_b:
-	add x15, x15, t1
+	add x15, x15, t5
 	jal x0, mult
 
 tens_b:	
-	add x15, x15, t1
+	add x15, x15, t5
 	addi x16, x16, 1
 	lw x6, ten
 	beq x16, x6, mrg_2b
 	jal x0, tens_b
 
 mrg_2b:
-	add x15, x15, t2
+	add x15, x15, t6
 	jal x0, mult
 
 hund_b:
-	add x15, x15, t1
+	add x15, x15, t5
 	addi x16, x16, 1
 	lw x6, one_hundred
 	beq x16, x6, tens_b2
 	jal x0, hund_b
 
 tens_b2:
-	add x17, x17, t2
+	add x17, x17, t6
 	addi x18, x18, 1
 	lw x6, ten
 	beq x17, x6, mrg_3b
@@ -206,25 +206,25 @@ tens_b2:
 
 mrg_3b:
 	add x15, x15, x17
-	add x15, x15, t3
+	add x15, x15, t5
 	jal x0, mult
 
 thous_b:
-	add x15, x15, t1
+	add x15, x15, t5
 	addi x16, x16, 1
 	lw x6, one_thousnd
 	beq x10, x6, hund_b2
 	jal x0, thous_b
 
 hund_b2:
-	add x17, x17, t3
+	add x17, x17, t5
 	addi x18, x18, 1
 	lw x6, one_hundred
 	beq x12, x6, tens_b3
 	jal x0, hund_b2
 
 tens_b3:
-	add x19, x19, t4
+	add x19, x19, t6
 	addi x20, x20, 1
 	lw x6, ten
 	beq x20, x6, mrg_4b
@@ -233,7 +233,7 @@ tens_b3:
 mrg_4b:
 	add x15, x15, x17
 	add x15, x15, x19
-	add x15, x15, t4
+	add x15, x15, t6
 	jal x0, mult
 
 mult:
@@ -244,6 +244,8 @@ mult:
 	jal x0, mult 
 
 print:
+	addi x21, x21, 48
+	sb x21, 1024(x0)
 	halt
 
 enter: .word 13
@@ -256,13 +258,14 @@ four: .word 4
 ten: .word 10
 one_hundred: .word 100
 one_thousnd: .word 1000
-a1: .word 0 
-a2: .word 0
-a3: .word 0
-a4: .word 0
-t1: .word 0
-t2: .word 0
-t3: .word 0
-t4: .word 0
+s6: .word 0 
+s7: .word 0
+s8: .word 0
+s9: .word 0
+t5: .word 0
+t6: .word 0
+t5: .word 0
+t6: .word 0
+
 
 
