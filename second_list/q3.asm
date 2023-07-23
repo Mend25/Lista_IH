87221e0dc8aba11bd36ed146a373556fc3ef79c8
Mend25 <113634496+Mend25@users.noreply.github.com>
2023-07-23 04:23:50 -0300

    Update q3.asm

--- second_list/q3.asm
+++ second_list/q3.asm
@@ -1,34 +1,35 @@
-.data
-vogais: .string "aeiouAEIOU"
-.text
 start:
     jal x1, read
+	l1:	
     sub x11, x11, x12
     jal x1, print
+	l2:	
     halt
 print:
-    div x5, x11, 10
-    sb x5, 1024(x0)
-    rem x5, x11, 10
-    sb x5, 1024(x0)
-    ret
+	addi x11, x11, 65
+	sb x11, 1024(x0)
+    jal x0, l2
 read:  
-    la x5, vogais
+    lw x5, vogais
     lb x10, 1025(x0)
-    bne x10, 10, count
-    ret
+	lw x7, enter
+    bne x10, x7, count
+    jal x0, l1
 count:
     addi x11, x11, 1 #contador de letras
-    mv x7, 0
+
 is_vog:
     lb x6, 0(x5)
     beq x10, x6, vog #verifica se eh vogal pela string vogais
-    addi x6, x6, 1
-    bne x6, 0, is_vog
+    addi x5, x5, 1
+	lw x7, zero
+    bne x6, x7, is_vog
     jal x0, read
 vog:
     addi x12, x12, 1 #contador de vogais
     jal x0, read
 
-
-    
+vogais: .asciz "aeiouAEIOU"
+zero: .word 0
+ten: .word 10
+enter: .word 13
