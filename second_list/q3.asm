5ac376ad8f4f86fda9008415b1391a79c44b5dfe
Vitor Manoel <114445111+vitork15@users.noreply.github.com>
2023-07-23 15:40:16 -0300

    Update q3.asm

--- second_list/q3.asm
+++ second_list/q3.asm
@@ -10,20 +10,33 @@ print:
 	sb x11, 1024(x0)
     jal x0, l2
 read:  
-    lui x5, 0x40000
     lb x10, 1025(x0)
 	lw x7, enter
     bne x10, x7, count
     jal x0, l1
 count:
     addi x11, x11, 1 #contador de letras
-
 is_vog:
-    lb x6, 0(x5)
-    beq x10, x6, vog #verifica se eh vogal pela string vogais
-    addi x5, x5, 1
-	lw x7, zero
-    bne x6, x7, is_vog
+    lw x6, a
+    beq x10, x6, vog
+    lw x6, b
+    beq x10, x6, vog
+    lw x6, c
+    beq x10, x6, vog
+    lw x6, d
+    beq x10, x6, vog
+    lw x6, e
+    beq x10, x6, vog
+    lw x6, am
+    beq x10, x6, vog
+    lw x6, bm
+    beq x10, x6, vog
+    lw x6, cm
+    beq x10, x6, vog
+    lw x6, dm
+    beq x10, x6, vog
+    lw x6, em
+    beq x10, x6, vog
     jal x0, read
 vog:
     addi x12, x12, 1 #contador de vogais
@@ -33,3 +46,55 @@ vog:
 zero: .word 0
 ten: .word 10
 enter: .word 13
+a: .word 'a'
+b: .word 'b'
+c: .word 'c'
+d: .word 'd'
+e: .word 'e'
+f: .word 'f'
+g: .word 'g'
+h: .word 'h'
+i: .word 'i'
+j: .word 'j'
+k: .word 'k'
+l: .word 'l'
+m: .word 'm'
+n: .word 'n'
+o: .word 'o'
+p: .word 'p'
+q: .word 'q'
+r: .word 'r'
+s: .word 's'
+t: .word 't'
+u: .word 'u'
+v: .word 'v'
+w: .word 'w'
+x: .word 'x'
+y: .word 'y'
+z: .word 'z'
+am: .word 'A'
+bm: .word 'B'
+cm: .word 'C'
+dm: .word 'D'
+em: .word 'E'
+fm: .word 'F'
+gm: .word 'G'
+hm: .word 'H'
+im: .word 'I'
+jm: .word 'J'
+km: .word 'K'
+lm: .word 'L'
+mm: .word 'M'
+nm: .word 'N'
+om: .word 'O'
+pm: .word 'P'
+qm: .word 'Q'
+rm: .word 'R'
+sm: .word 'S'
+tm: .word 'T'
+um: .word 'U'
+vm: .word 'V'
+wm: .word 'W'
+xm: .word 'X'
+ym: .word 'Y'
+zm: .word 'Z'
