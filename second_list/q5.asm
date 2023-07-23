3a31d9d4c1bee1c9e629b0ebc97418f63a1193b2
Mend25 <113634496+Mend25@users.noreply.github.com>
2023-07-23 07:44:35 -0300

    Consertei alguns erros

--- second_list/q5.asm
+++ second_list/q5.asm
@@ -1,6 +1,6 @@
 lw x9 , zero
 lw x10, zero
-lw x11, zero
+lw x11, zero 
 lw x12, zero
 lw x13, zero
 lw x14, zero
@@ -13,14 +13,14 @@ lw x20, zero
 lw x21, zero
 
 read_a:
+	lw x6, enter
+	beq x5, x6, prep_a
 	lb x5, 1025(x0)
 	addi x7, x7, 1
 	lw x6, neg
 	beq x5, x6, neg_a
 	lw x6, one
 	beq x6, x7, save_1
-	lw x6, enter
-	beq x5, x6, prep_a
 	lw x6, two
 	beq x6, x7, save_2
 	lw x6, three
@@ -105,14 +105,14 @@ thous_a:
 	jal x0, thous_a
 
 hund_s7:
-	add x11, x11, s8
+	add x11, x11, s7
 	addi x12, x12, 1
 	lw x6, one_hundred
 	beq x12, x6, tens_s8
 	jal x0, hund_s7
 
 tens_s8:
-	add x13, x13, s9
+	add x13, x13, s8
 	addi x14, x14, 1
 	lw x6, ten
 	beq x14, x6, mrg_4a
@@ -126,13 +126,13 @@ mrg_4a:
 
 read_b:	
 	lb x5, 1025(x0)
+	lw x6, enter
+	beq x5, x6, prep_b
 	addi x8, x8, 1
 	lw x6, neg
 	beq x5, x6, neg_b
 	lw x6, one
-	beq x6, x8, save_1b
-	lw x6, enter
-	beq x5, x6, prep_b
+	beq x6, x8, save_1
 	lw x6, two
 	beq x6, x8, save_2b
 	lw x6, three
@@ -147,22 +147,22 @@ neg_b:
 
 save_1b:
 	addi x5, x5, -48
-	sw x5, t5
+	sw x5, s6
 	jal x0, read_b
 
 save_2b:
 	addi x5, x5, -48
-	sw x5, t6
+	sw x5, s7
 	jal x0, read_b
 
 save_3b:
 	addi x5, x5, -48
-	sw x5, t5
+	sw x5, s8
 	jal x0, read_b
 
 save_4b:
 	addi x5, x5, -48
-	sw x5, t6
+	sw x5, s9
 	jal x0, read_b
 
 prep_b:
@@ -176,29 +176,29 @@ prep_b:
 	beq x6, x8, thous_b
 
 unit_b:
-	add x15, x15, t5
+	add x15, x15, s6
 	jal x0, mult
 
 tens_b:	
-	add x15, x15, t5
+	add x15, x15, s6
 	addi x16, x16, 1
 	lw x6, ten
 	beq x16, x6, mrg_2b
 	jal x0, tens_b
 
 mrg_2b:
-	add x15, x15, t6
+	add x15, x15, s7
 	jal x0, mult
 
 hund_b:
-	add x15, x15, t5
+	add x15, x15, s6
 	addi x16, x16, 1
 	lw x6, one_hundred
 	beq x16, x6, tens_b2
 	jal x0, hund_b
 
 tens_b2:
-	add x17, x17, t6
+	add x17, x17, s7
 	addi x18, x18, 1
 	lw x6, ten
 	beq x17, x6, mrg_3b
@@ -206,25 +206,25 @@ tens_b2:
 
 mrg_3b:
 	add x15, x15, x17
-	add x15, x15, t5
+	add x15, x15, s8
 	jal x0, mult
 
 thous_b:
-	add x15, x15, t5
+	add x15, x15, s6
 	addi x16, x16, 1
 	lw x6, one_thousnd
 	beq x10, x6, hund_b2
 	jal x0, thous_b
 
 hund_b2:
-	add x17, x17, t5
+	add x17, x17, s7
 	addi x18, x18, 1
 	lw x6, one_hundred
 	beq x12, x6, tens_b3
 	jal x0, hund_b2
 
 tens_b3:
-	add x19, x19, t6
+	add x19, x19, s8
 	addi x20, x20, 1
 	lw x6, ten
 	beq x20, x6, mrg_4b
@@ -233,7 +233,7 @@ tens_b3:
 mrg_4b:
 	add x15, x15, x17
 	add x15, x15, x19
-	add x15, x15, t6
+	add x15, x15, s9
 	jal x0, mult
 
 mult:
@@ -266,6 +266,7 @@ t5: .word 0
 t6: .word 0
 t5: .word 0
 t6: .word 0
+
 
 
 
